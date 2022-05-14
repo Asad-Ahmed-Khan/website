451381b48392fe3c2887d5ce7c5a982572b0946b
Asad Ahmed khan <kasad5620@gnmail.com>
2022-05-14 18:36:08 +0500

    update for testing git

--- EcommercsWithReactAndFirebase-main/src/App.js
+++ EcommercsWithReactAndFirebase-main/src/App.js
@@ -33,7 +33,7 @@ export class App extends Component {
             else {
                 this.setState({
                     user: null,
-                    userId: null
+                    userId: null,
                 })
             }
         })
--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(
 );
 
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); 
 
 app.use(
   cors({
